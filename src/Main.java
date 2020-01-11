d2a49e222452691fc165b5b17acfbc2bfd80202b
zhugeqinmoLv99 <2103885842@qq.coom>
2020-01-11 17:25:32 +0800

    更新测试4

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("测试中");
-        System.out.println("更新代码");
-        System.out.println("更新代码3");
     }
 }
